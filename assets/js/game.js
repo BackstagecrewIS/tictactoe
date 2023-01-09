b719396158aaa8b0bbc2c2642fa2200212d1c3d8
BackstagecrewIS <locksmith@backstagecrew.com>
2023-01-09 19:45:08 +0000

    Add gameOver functionn and messages

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,24 +2,25 @@
 
 // ------- All possible winning combinations -------
 const wins = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
 ];
 
-let oWins = 0;          // Count of player O wins
-let xWins = 0;          // Count of player X wins
-let gamesPlayed = 0;    // Count of all games played
-let omoves = [];        // Define empty player O moves array
-let xmoves = [];        // Define empty player X moves array
-let playedMoves = [];   // Define empty moves array
-let gameOver = false;   // End of game check
-let playerTurn = "O";   // Set the player O to begin the first game
+let oWins = 0; // Count of player O wins
+let xWins = 0; // Count of player X wins
+let gamesPlayed = 0; // Count of all games played
+let omoves = []; // Define empty player O moves array
+let xmoves = []; // Define empty player X moves array
+let playedMoves = []; // Define empty moves array
+let gameOver = false; // End of game check
+let playerTurn = "O"; // Set the player O to begin the first game
+let playedSquare;
 
 // ---------------------------------------- New Game
 
@@ -29,9 +30,9 @@ function newGame() {
   addGameCounter();
   gameOver = false;
   if (gamesPlayed % 2 == 0) {
-    playerTurn = "X";
+      playerTurn = "X";
   } else {
-    playerTurn = "O";
+      playerTurn = "O";
   }
   displayPlayerTurn(playerTurn);
 }
@@ -41,13 +42,13 @@ function newGame() {
 function clearBoard() {
   let squares = document.getElementsByClassName('square');
   for (let square of squares) {
-    square.backgroundColor = "white";
-    square.color = "black";
-    square.innerHTML = "";
-    square.style.color = "black";
-    square.style.fontWeight = "400";
-    };
-  };
+      square.backgroundColor = "white";
+      square.color = "black";
+      square.innerHTML = "";
+      square.style.color = "black";
+      square.style.fontWeight = "400";
+  }
+}
 
 // ---------------------------------------- Empty move arrays
 
@@ -60,80 +61,101 @@ function emptyArrays() {
 // ---------------------------------------- Increment game counter
 
 function addGameCounter() {
-  gamesPlayed ++;
+  gamesPlayed++;
 }
 
 // ---------------------------------------- Display player turn
 
 function displayPlayerTurn(player) {
-    let moveDisplay = document.getElementById('playerToPlay');
+  let moveDisplay = document.getElementById('playerToPlay');
   if (!gameOver) {
-    moveDisplay.innerHTML=`${player} to play`;
+      moveDisplay.innerHTML = `${player} to play`;
   }
 
 }
 
-// ---------------------------------------- Player move
+// ---------------------------------------- Start Game
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   displayPlayerTurn(playerTurn);
-  let squares = document.getElementsByClassName('square');
-  for (let square of squares) {
-    square.addEventListener("click", function() {
-    playedSquare = parseInt(square.id);
-
-      if (playerTurn == "O") {
-        if (checkMove(playedSquare)) {
-          this.textContent = "O";
-          omoves.push(playedSquare);
-          checkForWin();
-        };
-      } else {
-        if (checkMove(playedSquare)) {
-          this.textContent = "X";
-          xmoves.push(playedSquare);
-          checkForWin();
-        };
-      };
-    });
-
-// ---------------------------------------- Highlight cursor
-
-          square.addEventListener("mouseover", function() {
-              this.style.backgroundColor = "pink";
-          })
+  initialiseListeners();
   
-          square.addEventListener("mouseout", function() {
-              this.style.backgroundColor = "white";
-          })
-      };
 // ---------------------------------------- Button listeners
 
-      let buttons = document.getElementsByClassName('control');
-      for (let button of buttons) {
-          button.addEventListener("click", function() {
-              if (this.getAttribute("data-type") === "new-game") {
-                  newGame();
-              } else if (this.getAttribute("data-type") === "instructions") {
-                $('#modal').modal('show');
-              } else if (this.getAttribute("data-type") === "reset") {
-                  resetScores();
-              } else {
-                  console.log("BUTTON ERROR");
-              };
+let buttons = document.getElementsByClassName('control');
+for (let button of buttons) {
+  button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "new-game") {
+          newGame();
+      } else if (this.getAttribute("data-type") === "instructions") {
+          $('#modal').modal('show');
+      } else if (this.getAttribute("data-type") === "reset") {
+          resetScores();
+      }
+  });
+}
   
-          }); 
-      };   
-});
+})
+
+
+// ----------------------------------------- Initialise Listeners
+
+function initialiseListeners() {
+  let squares = document.getElementsByClassName('square');
+  for (let square of squares) {
+      square.addEventListener("click", function () {
+          playedSquare = parseInt(square.id);
+          if (playerTurn == "O") {
+              if (checkMove(playedSquare)) {
+                  this.textContent = "O";
+                  omoves.push(playedSquare);
+                  checkForWin();
+              }
+          } else {
+              if (checkMove(playedSquare)) {
+                  this.textContent = "X";
+                  xmoves.push(playedSquare);
+                  checkForWin();
+              }
+          }
+      });
+// ---------------------------------------- Highlight cursor 
+
+      square.addEventListener("mouseover", function () {
+          this.style.backgroundColor = "pink";
+      })
+      square.addEventListener("mouseout", function () {
+          this.style.backgroundColor = "white";
+      })
+  }
+}
+
+// ----------------------------------------- Remove Listeners
+
+function removeListeners() {
+  let squares = document.getElementsByClassName('square');
+  for (let square of squares) {
+      square.removeEventListener("click", function () {})
+
+      // ---------------------------------------- Remove  Cursor Highlight
+
+      square.addEventListener("mouseover", function () {
+          this.style.backgroundColor = "white";
+      })
+      square.addEventListener("mouseout", function () {
+          this.style.backgroundColor = "white";
+      })
+  }
+}
 
 // ---------------------------------------- Check for valid move
 
 function checkMove(move) {
-  if (!playedMoves.includes(move)) {
-    addPlayedMove(move);
-    return true;
+  if (!playedMoves.includes(move) && !gameOver) {
+      addPlayedMove(move);
+      return true;
   } else {
-    return false;
+      return false;
   }
 }
 
@@ -151,23 +173,16 @@ function checkForWin() {
           let winningLine = win;
           displayWinningLine(winningLine);
           addOWin();
-        
           gameOver = true;
-  
           gameEnd("O wins");
       }
-      
       if (checkSubset(xmoves, win)) {
-        let winningLine = win;
-        displayWinningLine(winningLine);
-        addXWin();
-        
-        gameOver = true;
-
-        gameEnd("X wins");
+          let winningLine = win;
+          displayWinningLine(winningLine);
+          addXWin();
+          gameOver = true;
+          gameEnd("X wins");
       }
-
-      
   }
   checkForDraw();
   changePlayerTurn();
@@ -175,20 +190,16 @@ function checkForWin() {
 
 // ---------------------------------------- Check for Draw
 function checkForDraw() {
-  console.log("Game Over? : ", gameOver);
-  console.log("MOVES: ", playedMoves.length);
   if (!gameOver && playedMoves.length == 9) {
-        
-    gameOver = true;
-
-    gameEnd("It's a draw");
+      gameOver = true;
+      gameEnd("It's a draw");
   }
 }
 
 
 // ---------------------------------------- Check moves against winning combinations
 
-function checkSubset (movesArray, winsArray) {
+function checkSubset(movesArray, winsArray) {
   return winsArray.every((element) => {
       return movesArray.includes(element)
   })
@@ -209,20 +220,20 @@ function changePlayerTurn() {
 
 function displayWinningLine(line) {
   for (let marker of line) {
-    document.getElementById(marker).style.color = "red";
-    document.getElementById(marker).style.fontWeight = "700";
+      document.getElementById(marker).style.color = "red";
+      document.getElementById(marker).style.fontWeight = "700";
   }
 }
 
 // ---------------------------------------- Add to winner total
 
 function addOWin() {
-  oWins ++;
+  oWins++;
   document.getElementById("o-wins").innerText = "O wins: " + oWins;
 }
 
 function addXWin() {
-  xWins ++;
+  xWins++;
   document.getElementById("x-wins").innerText = "X wins: " + xWins;
 }
 
@@ -230,15 +241,16 @@ function addXWin() {
 
 function displayWinTotals() {
   let oTotal = document.getElementById('o-wins');
-  oTotal.innerHTML=`O wins : ${oWins} `;
+  oTotal.innerHTML = `O wins : ${oWins} `;
   let xTotal = document.getElementById('x-wins');
-  xTotal.innerHTML=`X wins : ${xWins} `;
-  }
-  
+  xTotal.innerHTML = `X wins : ${xWins} `;
+}
+
 // ---------------------------------------- Game end messages
 
 function gameEnd(result) {
   let message = document.getElementById('playerToPlay');
+  removeListeners();
   message.innerHTML = result;
 }
 
